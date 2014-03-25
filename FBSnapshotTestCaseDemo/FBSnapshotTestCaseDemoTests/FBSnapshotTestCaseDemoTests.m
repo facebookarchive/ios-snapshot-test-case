a69f90fb6e2e391f2b2242f1d004c63a41180ea5
Florian Goessler <mail@floriangoessler.de>
2014-03-25 17:47:01 +0100

    changed outdated comment in test example

--- FBSnapshotTestCaseDemo/FBSnapshotTestCaseDemoTests/FBSnapshotTestCaseDemoTests.m
+++ FBSnapshotTestCaseDemo/FBSnapshotTestCaseDemoTests/FBSnapshotTestCaseDemoTests.m
@@ -22,7 +22,7 @@
   [super setUp];
   // Flip this to YES to record images in the reference image directory.
   // You need to do this the first time you create a test and whenever you change the snapshotted views.
-  // Be careful not to commit with recordMode on though, or your tests will never fail.
+  // Tests running in record mode will allways fail so that you know that you have to do something here before you commit.
   self.recordMode = NO;
 }
 
