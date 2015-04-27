323ea9074276fa59defd52335756e1c67ace853b
Patrik Tomas Chamelo <patrick@nscoding.co.uk>
2015-04-26 23:19:06 -0700

    Changes based on discussion

--- FBSnapshotTestCase/FBSnapshotTestCase.h
+++ FBSnapshotTestCase/FBSnapshotTestCase.h
@@ -8,7 +8,7 @@
  *
  */
 
-#import <FBSnapshotTestCase/FBSnapshotTestCaseArchitecture.h>
+#import <FBSnapshotTestCase/FBSnapshotTestCasePlatform.h>
 
 #import <QuartzCore/QuartzCore.h>
 
@@ -24,18 +24,17 @@
  Similar to our much-loved XCTAssert() macros. Use this to perform your test. No need to write an explanation, though.
  @param view The view to snapshot
  @param identifier An optional identifier, used if there are multiple snapshot tests in a given -test method.
- @param version An optional suffix, appended to the reference image directory path, such as "_iOS8"
- @param architectureSuffixes An optional dictionary with a type and suffix e.g. @{ FBSnapshotTestArchitectureType64BitKey : @"_64"}
+ @param suffixes An NSOrderedSet of strings for the different suffixes
  */
-#define FBSnapshotVerifyViewWithOptions(view__, identifier__, version__, architectureSuffixes__) \
+#define FBSnapshotVerifyViewWithOptions(view__, identifier__, suffixes__) \
 { \
-NSOrderedSet *suffixes__ = FBSuffixesForArchitectureTypes(architectureSuffixes__); \
 NSError *error__ = nil; \
 BOOL comparisonSuccess__; \
+XCTAssertTrue((suffixes__.count > 0), @"Suffixes set cannot be empty %@", suffixes__); \
 for (NSString *suffix__ in suffixes__) { \
-NSString *referenceImagesDirectory__ = [NSString stringWithFormat:@"%s%@%@", FB_REFERENCE_IMAGE_DIR, version__, suffix__]; \
+NSString *referenceImagesDirectory__ = [NSString stringWithFormat:@"%s%@", FB_REFERENCE_IMAGE_DIR, suffix__]; \
 comparisonSuccess__ = [self compareSnapshotOfView:(view__) referenceImagesDirectory:referenceImagesDirectory__ identifier:(identifier__) error:&error__]; \
-if (comparisonSuccess__) break; \
+if (comparisonSuccess__ || self.recordMode) break; \
 } \
 XCTAssertTrue(comparisonSuccess__, @"Snapshot comparison failed: %@", error__); \
 XCTAssertFalse(self.recordMode, @"Test ran in record mode. Reference image is now saved. Disable record mode to perform an actual snapshot comparison!"); \
@@ -43,29 +42,24 @@ XCTAssertFalse(self.recordMode, @"Test ran in record mode. Reference image is no
 
 #define FBSnapshotVerifyView(view__, identifier__) \
 { \
-NSDictionary *architectureSuffixes__ = @{ \
-FBSnapshotTestArchitectureType32BitKey : @"_32", \
-FBSnapshotTestArchitectureType64BitKey : @"_64", \
-}; \
-FBSnapshotVerifyViewWithOptions(view__, identifier__, @"", architectureSuffixes__); \
+FBSnapshotVerifyViewWithOptions(view__, identifier__, FBSnapshotTestCaseDefaultSuffixes()); \
 }
 
 /**
  Similar to our much-loved XCTAssert() macros. Use this to perform your test. No need to write an explanation, though.
  @param layer The layer to snapshot
  @param identifier An optional identifier, used is there are multiple snapshot tests in a given -test method.
- @param version An optional suffix, appended to the reference image directory path, such as "_iOS8"
- @param architectureTypes An optional dictionary with a type and suffix e.g. @{ FBSnapshotTestArchitectureType64BitKey : @"_64"}
+ @param suffixes An NSOrderedSet of strings for the different suffixes
  */
-#define FBSnapshotVerifyLayerWithOptions(layer__, identifier__, version__, architectureTypes__) \
+#define FBSnapshotVerifyLayerWithOptions(layer__, identifier__, suffixes__) \
 { \
-NSOrderedSet *suffixes__ = FBSuffixesForArchitectureTypes(architectureTypes__); \
 NSError *error__ = nil; \
 BOOL comparisonSuccess__; \
+XCTAssertTrue((suffixes__.count > 0), @"Suffixes set cannot be empty %@", suffixes__); \
 for (NSString *suffix__ in suffixes__) { \
-NSString *referenceImagesDirectory__ = [NSString stringWithFormat:@"%s%@%@", FB_REFERENCE_IMAGE_DIR, version__, suffix__]; \
+NSString *referenceImagesDirectory__ = [NSString stringWithFormat:@"%s%@", FB_REFERENCE_IMAGE_DIR, suffix__]; \
 comparisonSuccess__ = [self compareSnapshotOfLayer:(layer__) referenceImagesDirectory:referenceImagesDirectory__ identifier:(identifier__) error:&error__]; \
-if (comparisonSuccess__) break; \
+if (comparisonSuccess__ || self.recordMode) break; \
 } \
 XCTAssertTrue(comparisonSuccess__, @"Snapshot comparison failed: %@", error__); \
 XCTAssertFalse(self.recordMode, @"Test ran in record mode. Reference image is now saved. Disable record mode to perform an actual snapshot comparison!"); \
@@ -73,19 +67,23 @@ XCTAssertFalse(self.recordMode, @"Test ran in record mode. Reference image is no
 
 #define FBSnapshotVerifyLayer(layer__, identifier__) \
 { \
-NSDictionary *architectureSuffixes__ = @{ \
-FBSnapshotTestArchitectureType32BitKey : @"_32", \
-FBSnapshotTestArchitectureType64BitKey : @"_64", \
-}; \
-FBSnapshotVerifyLayerWithOptions(layer__, identifier__, @"", architectureSuffixes__); \
+FBSnapshotVerifyLayerWithOptions(layer__, identifier__, FBSnapshotTestCaseDefaultSuffixes()); \
 }
 
 /**
  The base class of view snapshotting tests. If you have small UI component, it's often easier to configure it in a test
  and compare an image of the view to a reference image that write lots of complex layout-code tests.
  
- In order to flip the tests in your subclass to record the reference images set `recordMode` to YES before calling
- -[super setUp].
+ In order to flip the tests in your subclass to record the reference images set @c recordMode to @c YES.
+ 
+ For example:
+ @code
+ - (void)setUp
+ {
+    [super setUp];
+    self.recordMode = YES;
+ }
+ @endcode
  */
 @interface FBSnapshotTestCase : XCTestCase
 
@@ -99,7 +97,7 @@ FBSnapshotVerifyLayerWithOptions(layer__, identifier__, @"", architectureSuffixe
  @param layer The Layer to snapshot
  @param referenceImagesDirectory The directory in which reference images are stored.
  @param identifier An optional identifier, used if there are multiple snapshot tests in a given -test method.
- @param error An error to log in an XCTAssert() macro if the method fails (missing reference image, images differ, etc).
+ @param errorPtr An error to log in an XCTAssert() macro if the method fails (missing reference image, images differ, etc).
  @returns YES if the comparison (or saving of the reference image) succeeded.
  */
 - (BOOL)compareSnapshotOfLayer:(CALayer *)layer
@@ -112,7 +110,7 @@ FBSnapshotVerifyLayerWithOptions(layer__, identifier__, @"", architectureSuffixe
  @param view The view to snapshot
  @param referenceImagesDirectory The directory in which reference images are stored.
  @param identifier An optional identifier, used if there are multiple snapshot tests in a given -test method.
- @param error An error to log in an XCTAssert() macro if the method fails (missing reference image, images differ, etc).
+ @param errorPtr An error to log in an XCTAssert() macro if the method fails (missing reference image, images differ, etc).
  @returns YES if the comparison (or saving of the reference image) succeeded.
  */
 - (BOOL)compareSnapshotOfView:(UIView *)view
