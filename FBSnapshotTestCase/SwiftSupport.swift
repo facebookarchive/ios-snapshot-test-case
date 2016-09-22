f9f89db5309cf4332422f3840402a32ce283e859
Patrick Chamelo <nscoding@users.noreply.github.com>
2016-09-22 10:58:57 -0700

    Merge Swift 3 in master (#194)
    * Add support for Swift 3
    
    This keeps support for Swift 2.x.
    
    * Update demo app for Xcode 8
    
    - Updated Swift sample code
    - Podfile includes CocoaPods workaround to set SWIFT_VERSION
    - Cocoapods version used is 1.0.1
    - Updated reference image for slight differences in OS gradient

--- FBSnapshotTestCase/SwiftSupport.swift
+++ FBSnapshotTestCase/SwiftSupport.swift
@@ -8,6 +8,64 @@
 *
 */
 
+#if swift(>=3)
+  public extension FBSnapshotTestCase {
+    public func FBSnapshotVerifyView(_ view: UIView, identifier: String = "", suffixes: NSOrderedSet = FBSnapshotTestCaseDefaultSuffixes(), tolerance: CGFloat = 0, file: StaticString = #file, line: UInt = #line) {
+      FBSnapshotVerifyViewOrLayer(view, identifier: identifier, suffixes: suffixes, tolerance: tolerance, file: file, line: line)
+    }
+
+    public func FBSnapshotVerifyLayer(_ layer: CALayer, identifier: String = "", suffixes: NSOrderedSet = FBSnapshotTestCaseDefaultSuffixes(), tolerance: CGFloat = 0, file: StaticString = #file, line: UInt = #line) {
+      FBSnapshotVerifyViewOrLayer(layer, identifier: identifier, suffixes: suffixes, tolerance: tolerance, file: file, line: line)
+    }
+
+    private func FBSnapshotVerifyViewOrLayer(_ viewOrLayer: AnyObject, identifier: String = "", suffixes: NSOrderedSet = FBSnapshotTestCaseDefaultSuffixes(), tolerance: CGFloat = 0, file: StaticString = #file, line: UInt = #line) {
+      let envReferenceImageDirectory = self.getReferenceImageDirectory(withDefault: FB_REFERENCE_IMAGE_DIR)
+      var error: NSError?
+      var comparisonSuccess = false
+
+      if let envReferenceImageDirectory = envReferenceImageDirectory {
+        for suffix in suffixes {
+          let referenceImagesDirectory = "\(envReferenceImageDirectory)\(suffix)"
+          if viewOrLayer.isKind(of: UIView.self) {
+            do {
+              try compareSnapshot(of: viewOrLayer as! UIView, referenceImagesDirectory: referenceImagesDirectory, identifier: identifier, tolerance: tolerance)
+              comparisonSuccess = true
+            } catch let error1 as NSError {
+              error = error1
+              comparisonSuccess = false
+            }
+          } else if viewOrLayer.isKind(of: CALayer.self) {
+            do {
+              try compareSnapshot(of: viewOrLayer as! CALayer, referenceImagesDirectory: referenceImagesDirectory, identifier: identifier, tolerance: tolerance)
+              comparisonSuccess = true
+            } catch let error1 as NSError {
+              error = error1
+              comparisonSuccess = false
+            }
+          } else {
+            assertionFailure("Only UIView and CALayer classes can be snapshotted")
+          }
+
+          assert(recordMode == false, message: "Test ran in record mode. Reference image is now saved. Disable record mode to perform an actual snapshot comparison!", file: file, line: line)
+
+          if comparisonSuccess || recordMode {
+            break
+          }
+
+          assert(comparisonSuccess, message: "Snapshot comparison failed: \(error)", file: file, line: line)
+        }
+      } else {
+        XCTFail("Missing value for referenceImagesDirectory - Set FB_REFERENCE_IMAGE_DIR as Environment variable in your scheme.")
+      }
+    }
+
+    func assert(_ assertion: Bool, message: String, file: StaticString, line: UInt) {
+      if !assertion {
+        XCTFail(message, file: file, line: line)
+      }
+    }
+  }
+#else
 public extension FBSnapshotTestCase {
   public func FBSnapshotVerifyView(view: UIView, identifier: String = "", suffixes: NSOrderedSet = FBSnapshotTestCaseDefaultSuffixes(), tolerance: CGFloat = 0, file: StaticString = #file, line: UInt = #line) {
     FBSnapshotVerifyViewOrLayer(view, identifier: identifier, suffixes: suffixes, tolerance: tolerance, file: file, line: line)
@@ -64,3 +122,4 @@ public extension FBSnapshotTestCase {
     }
   }
 }
+#endif
--- FBSnapshotTestCaseDemo/FBSnapshotTestCaseDemoTests/FBSnapshotTestCaseSwiftTests.swift
+++ FBSnapshotTestCaseDemo/FBSnapshotTestCaseDemoTests/FBSnapshotTestCaseSwiftTests.swift
@@ -18,7 +18,7 @@ class FBSnapshotTestCaseSwiftTest: FBSnapshotTestCase {
 
   func testExample() {
     let view = UIView(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
-    view.backgroundColor = UIColor.blueColor()
+    view.backgroundColor = UIColor.blue
     FBSnapshotVerifyView(view)
     FBSnapshotVerifyLayer(view.layer)
   }
